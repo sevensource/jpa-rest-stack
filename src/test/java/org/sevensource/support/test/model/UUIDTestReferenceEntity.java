b4095db32ab7b76e2546e9c116ba08e99b391c57
pgaschuetz <philipp@sevensource.org>
2017-03-19 19:26:25 +0100

    no message

--- src/test/java/org/sevensource/support/test/model/UUIDTestReferenceEntity.java
+++
@@ -1,9 +0,0 @@
-package org.sevensource.support.test.model;
-
-import javax.persistence.Entity;
-
-import org.sevensource.support.jpa.model.AbstractUUIDEntity;
-
-@Entity
-public class UUIDTestReferenceEntity extends AbstractUUIDEntity {
-}
