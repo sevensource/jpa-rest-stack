bc699e748728a188b2978c2e3598c00ebc7e940d
pgaschuetz <philipp@sevensource.org>
2018-11-19 17:25:07 +0100

    * fix tests

--- jpa-support/src/test/java/org/sevensource/support/jpa/filter/predicate/FilterCriteriaPredicateBuilderTest.java
+++ jpa-support/src/test/java/org/sevensource/support/jpa/filter/predicate/FilterCriteriaPredicateBuilderTest.java
@@ -3,6 +3,11 @@ package org.sevensource.support.jpa.filter.predicate;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.sevensource.support.jpa.configuration.JpaTestConfiguration;
@@ -13,6 +18,7 @@ import org.sevensource.support.jpa.filter.LogicalFilterCriteria;
 import org.sevensource.support.jpa.filter.LogicalFilterOperator;
 import org.sevensource.support.jpa.filter.predicate.domain.Customer;
 import org.sevensource.support.jpa.filter.predicate.domain.CustomerRepository;
+import org.sevensource.support.jpa.filter.predicate.domain.CustomerType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -27,6 +33,30 @@ import org.springframework.test.context.junit4.SpringRunner;
 @EntityScan(basePackageClasses=Customer.class)
 @EnableJpaRepositories(basePackageClasses=CustomerRepository.class)
 public class FilterCriteriaPredicateBuilderTest {
+	
+	Customer person1;
+	Customer person2;
+	Customer person3;
+	Customer company;
+	Customer enterprise;
+	
+	final Instant instant0 = Instant.ofEpochMilli(0);
+	final Instant instant2000 = ZonedDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneId.of("Z")).toInstant();
+	
+	final ZonedDateTime dateTime1900 = ZonedDateTime.of(1900, 12, 24, 20, 0, 0, 0, ZoneId.of("Z"));
+	final ZonedDateTime dateTime2000 = ZonedDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneId.of("Z"));
+	
+	@Before
+	public void before() {
+		
+		repository.deleteAllInBatch();
+		person1 = repository.save(new Customer("John", "Doe", CustomerType.PERSON, 35, instant0, dateTime1900));
+		person2 = repository.save(new Customer("Mary", "Blige", CustomerType.PERSON, 35, instant0, dateTime1900));
+		person3 = repository.save(new Customer(null, "Marks", null, 15, null, null));
+		company = repository.save(new Customer("Michael", "Huntington", CustomerType.COMPANY, 50, instant2000, dateTime2000));
+		enterprise = repository.save(new Customer("Bill", "Gates", CustomerType.COMPANY, 65, instant2000, dateTime2000));
+		repository.flush();
+	}
 	
 	@Autowired
 	CustomerRepository repository;
